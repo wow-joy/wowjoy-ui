5361a9d7b495fe4a54eef41e9fe645ff31b4db2e
likun <1617455243@qq.com>
2019-11-20 16:36:37 +0800

    fix: demo 展示

--- src/components/Dialog/index.js
+++ src/components/Dialog/index.js
@@ -1,6 +1,6 @@
 import Dialog from "./Dialog";
 import Hoc from "wowjoy-component/es/tools/Hoc";
-import DialogDark from "./types/Dark";
+import DialogDark from "./types/dark";
 import DialogConfirm from "./types/Confirm";
 
 export { DialogDark, DialogConfirm };
